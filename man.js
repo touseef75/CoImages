d338e23689a5b5490191562fc1afeacc4f2150c5
touseef75 <imrantouseef776@gmail.com>
2021-09-22 01:08:31 -0700

    call a images

--- man.js
+++ man.js
@@ -1,15 +1,37 @@
-function press(){
-    let values = document.getElementById("search").value;
-    let li = document.querySelectorAll("li");
-    for(let i of li){
-        let ival = i.innerHTML;
-        if(ival.indexOf(values) == -1){
-            i.classList.add("hide")
-        }else{
-            i.classList.remove("hide")
+// function press(){
+//     let values = document.getElementById("search").value;
+//     let li = document.querySelectorAll("li");
+//     for(let i of li){
+//         let ival = i.innerHTML;
+//         if(ival.indexOf(values) == -1){
+//             i.classList.add("hide")
+//         }else{
+//             i.classList.remove("hide")
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-press();
+// press();
+ const auth = "563492ad6f9170000100000192ce204c3d4d43f48315e56b93786c51";
+ async function images(){
+     const data = await fetch (`https://api.pexels.com/v1/curated?per_page=1`,
+     {
+         method:"GET",
+         headers:{
+             Accept : "application/json",
+             Authorization : auth,
+         },
+     }
+     )
+     const result = await data.json();
+     console.log(result)
+     result.photos.forEach(photo => {
+         const pic = document.createElement("div");
+     });
+     
+ }
+ images();
+
+
+
