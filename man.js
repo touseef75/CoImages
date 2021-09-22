91d227d7bfe8766f999335ea97dd24061632a74a
touseef75 <imrantouseef776@gmail.com>
2021-09-22 02:15:57 -0700

    get all images wih api

--- man.js
+++ man.js
@@ -15,7 +15,7 @@
 // press();
  const auth = "563492ad6f9170000100000192ce204c3d4d43f48315e56b93786c51";
  async function images(){
-     const data = await fetch (`https://api.pexels.com/v1/curated?per_page=1`,
+     const data = await fetch (`https://api.pexels.com/v1/curated?per_page=100`,
      {
          method:"GET",
          headers:{
@@ -28,6 +28,12 @@
      console.log(result)
      result.photos.forEach(photo => {
          const pic = document.createElement("div");
+         pic.innerHTML = `<img src=${photo.src.Nature}
+         <p> photo : ${photo.photographer}</p>
+         <a href = ${photo.src.Nature}>download</a>
+         `;
+         document.querySelector(".gallery").appendChild(pic);
+
      });
      
  }
